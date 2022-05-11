365329305eb81f58f7c464dbe4519642c20b4a99
Vladyslav Umerenko <space.vlad7@gmail.com>
2022-05-11 22:25:51 +0300

    serve static

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    server: {
+        port: Number(process.env.PORT || 3000),
+    },
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const serve = require('koa-static');
+const config = require('./config');
+
+const app = new Koa();
+
+app.use(serve('static'));
+
+const server = app.listen(config.server.port);
+
+app.on('error', err => {
+    console.error(err);
+
+    process.exit(1);
+});
+
+const shutdown = () => {
+    console.log('\nShutting down server');
+    server.close();
+    process.exit(0);
+}
+
+process.once('SIGTERM', shutdown);
+process.once('SIGINT', shutdown);
+
+console.info('Server listening to ' + config.server.port);
